b7698c17ff2071742c4e88a97259e059ddc5124b
KarenUrbano <quemojojojo@hotmail.com>
2022-03-21 12:31:52 -0500

    roles y variables de sesión

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 //import styles from '../styles/Home.module.css'
 import { useSession, signIn, signOut } from "next-auth/react"
+import Link from 'next/link'
 
 export default function Home() {
   const { data: session } = useSession()
   return (
     <div>
-      <p>{session?"Sesion iniciada":"Sin sesion"}</p>
+      <p>{session?"Sesion iniciada "+session?.user.name+" Role: "+session?.user.role:"Sin sesion"}</p>
       {console.log(session?.user)}
         <button onClick={() => signIn()}>Sign in</button>
         <button onClick={() => signOut()}>Sign out</button>
+        <Link href="/editor">Editor</Link>
     </div>
   )
 }
